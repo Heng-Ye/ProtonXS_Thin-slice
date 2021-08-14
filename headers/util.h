2b7d6eaee3ea4ace99b7564f86661e8eeba41bad
Heng-Ye Liao <hyliao@dunegpvm08.fnal.gov>
2021-08-13 23:21:55 -0500

    add fill hist. with weight

--- headers/util.h
+++ headers/util.h
@@ -11,3 +11,16 @@ void Fill1DHist(TH1D *hist, const double &value){
   }
 }
 
+
+void Fill1DWHist(TH1D *hist, const double &value, const double &wei){
+  if (value<hist->GetXaxis()->GetXmin()){
+    hist->Fill(hist->GetXaxis()->GetXmin(), wei);
+  }
+  else if (value<hist->GetXaxis()->GetXmax()){
+    hist->Fill(value, wei);
+  }
+  else{
+    hist->Fill(hist->GetXaxis()->GetXmax()-0.000001, wei);
+  }
+}
+
