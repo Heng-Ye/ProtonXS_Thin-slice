26e2fdee878592957776958d004feb2d15c4a5fe
Heng-Ye Liao <hyliao@dunegpvm08.fnal.gov>
2021-09-02 14:53:24 -0500

    Text style on y-axis of XS

--- make_plots/makeXS.C
+++ make_plots/makeXS.C
@@ -11,6 +11,8 @@ void makeXS(TString fin, TString outpath){
 	//gStyle->SetTitleX(0.5);
 	gStyle->SetTitleAlign(23); 
 	gStyle->SetOptStat(0);
+	gStyle->SetPadLeftMargin(0.13);
+
 
 	TFile *file = TFile::Open(fin.Data());
 	TH1D *eff_num_Int = (TH1D*)file->Get("eff_num_Int");
@@ -20,6 +22,8 @@ void makeXS(TString fin, TString outpath){
 	TH1D *pur_num_Int = (TH1D*)file->Get("pur_num_Int");
 	TH1D *pur_num_Inc = (TH1D*)file->Get("pur_num_Inc");
 	TH1D *pur_den = (TH1D*)file->Get("pur_den");
+	TH1D *pur_den_Inc = (TH1D*)file->Get("pur_den_Inc");
+	TH1D *pur_den_Int = (TH1D*)file->Get("pur_den_Int");
 
 	TH1D *reco_AngCorr = (TH1D*)file->Get("reco_AngCorr");
 	TH1D *true_AngCorr = (TH1D*)file->Get("true_AngCorr");
@@ -34,10 +38,10 @@ void makeXS(TString fin, TString outpath){
 	eff_Inc->Divide(eff_den_Inc);
 
 	TH1D *pur_Int = (TH1D*)pur_num_Int->Clone("pur_Int");
-	pur_Int->Divide(pur_den);
+	pur_Int->Divide(pur_den_Int);
 
 	TH1D *pur_Inc = (TH1D*)pur_num_Inc->Clone("pur_Inc");
-	pur_Inc->Divide(pur_den);
+	pur_Inc->Divide(pur_den_Inc);
 
 	TH1D *h_truesliceid_all = (TH1D*)file->Get("h_truesliceid_all");
 	TH1D *h_truesliceid_cuts = (TH1D*)file->Get("h_truesliceid_cuts");
@@ -46,23 +50,29 @@ void makeXS(TString fin, TString outpath){
 	TH1D *h_recosliceid_allevts_cuts = (TH1D*)file->Get("h_recosliceid_allevts_cuts");
 	TH1D *h_recosliceid_cuts = (TH1D*)file->Get("h_recosliceid_cuts");
 	TH1D *h_recosliceid_inelastic_cuts = (TH1D*)file->Get("h_recosliceid_inelastic_cuts");
+	TH1D *h_recosliceid_recoinelastic_cuts = (TH1D*)file->Get("h_recosliceid_recoinelastic_cuts");
 
 	TH1D *hinc = (TH1D*)h_recosliceid_allevts_cuts->Clone("hinc");
-	TH1D *hint = (TH1D*)h_recosliceid_allevts_cuts->Clone("hint");
+	//TH1D *hint = (TH1D*)h_recosliceid_allevts_cuts->Clone("hint");
+	TH1D *hint = (TH1D*)h_recosliceid_recoinelastic_cuts->Clone("hint");
 	hinc->Multiply(pur_Inc);
 
 
 
 	TCanvas *c1 = new TCanvas("c1","c1");
-	h_recosliceid_allevts_cuts->SetLineColor(3);
-	h_recosliceid_allevts_cuts->SetMarkerColor(3);
-	h_recosliceid_allevts_cuts->SetTitle("All Protons;Reco SliceID;Events");
+	h_recosliceid_allevts_cuts->SetLineColor(7);
+	h_recosliceid_allevts_cuts->SetMarkerColor(7);
+	//h_recosliceid_allevts_cuts->SetTitle("All Protons;Reco SliceID;Events");
+	h_recosliceid_allevts_cuts->SetTitle(";Reco SliceID;Events");
+	h_recosliceid_allevts_cuts->GetYaxis()->SetRangeUser(0,3000);
+	h_recosliceid_allevts_cuts->GetYaxis()->SetTitleOffset(1.3);
 	h_recosliceid_allevts_cuts->DrawCopy();
 	hinc->DrawCopy("same");
 	h_recosliceid_cuts->SetLineColor(2);
 	h_recosliceid_cuts->Draw("same hist");
 	//TLegend *leg1 = new TLegend(0.5,0.6,0.8,0.9);
-	TLegend *leg1 = new TLegend(0.5-0.3,0.6,0.8-0.3,0.9);
+	//TLegend *leg1 = new TLegend(0.5-0.3,0.6,0.8-0.3,0.9);
+	TLegend *leg1 = new TLegend(0.14,0.55,0.64,0.9);
 	leg1->SetFillStyle(0);
 	leg1->AddEntry(h_recosliceid_allevts_cuts,"Selected","ple");
 	leg1->AddEntry(hinc, "Selected #times purity","ple");
@@ -70,9 +80,12 @@ void makeXS(TString fin, TString outpath){
 	leg1->Draw();
 
 	TCanvas *c2 = new TCanvas("c2","c2");
-	h_truesliceid_uf->SetLineColor(4);
-	h_truesliceid_uf->SetMarkerColor(4);
-	h_truesliceid_uf->SetTitle("All Protons; True SliceID; Events");
+	h_truesliceid_uf->SetLineColor(7);
+	h_truesliceid_uf->SetMarkerColor(7);
+	//h_truesliceid_uf->SetTitle("All Protons; True SliceID; Events");
+	h_truesliceid_uf->SetTitle("; True SliceID; Events");
+	h_truesliceid_uf->GetYaxis()->SetRangeUser(0,4200);
+	h_truesliceid_uf->GetYaxis()->SetTitleOffset(1.3);
 	h_truesliceid_uf->Draw();
 	//hinc->SetLineColor(3);
 	//hinc->SetMarkerColor(3);
@@ -81,27 +94,46 @@ void makeXS(TString fin, TString outpath){
 	//  eff_den_Inc->SetMarkerColor(2);
 	//  eff_den_Inc->Draw("same hist");
 	h_truesliceid_all->SetLineColor(2);
+	h_truesliceid_all->SetLineWidth(4);
 	h_truesliceid_all->SetMarkerColor(2);
 	h_truesliceid_all->Draw("same hist");
 	//TLegend *leg2 = new TLegend(0.5,0.6,0.8,0.9);
-	TLegend *leg2 = new TLegend(0.5-0.3,0.6,0.8-0.3,0.9);
+	//TLegend *leg2 = new TLegend(0.5,0.6,0.8,0.9);
+	TLegend *leg2 = new TLegend(0.25,0.6,0.75,0.9);
 	leg2->SetFillStyle(0);
 	leg2->AddEntry(hinc, "Selected #times purity","ple");
 	leg2->AddEntry(h_truesliceid_uf,"Unfolded protons","ple");
 	leg2->AddEntry(h_truesliceid_all,"True protons","l");
 	leg2->Draw();
 
+
+
 	hint->Multiply(pur_Int);
+	float ymin_recoinel=0;
+	float ymax_recoinel=1900;
 	TCanvas *c3 = new TCanvas("c3","c3");
-	h_recosliceid_allevts_cuts->SetLineColor(3);
-	h_recosliceid_allevts_cuts->SetMarkerColor(3);
-	h_recosliceid_allevts_cuts->SetTitle("Proton Inelastic Scatterings;Reco SliceID;Events");
-	h_recosliceid_allevts_cuts->Draw();
-	hint->DrawCopy("same");
+	//h_recosliceid_allevts_cuts->SetLineColor(4);
+	//h_recosliceid_allevts_cuts->SetMarkerColor(4);
+	//h_recosliceid_allevts_cuts->SetTitle("Proton Inelastic Scatterings;Reco SliceID;Events");
+	//h_recosliceid_allevts_cuts->Draw();
+
+	h_recosliceid_recoinelastic_cuts->SetLineColor(7);
+	h_recosliceid_recoinelastic_cuts->SetMarkerColor(7);
+	//h_recosliceid_recoinelastic_cuts->SetTitle("Proton Inelastic Scatterings;Reco SliceID;Events");
+	h_recosliceid_recoinelastic_cuts->SetTitle(";Reco SliceID;Events");
+	h_recosliceid_recoinelastic_cuts->GetYaxis()->SetTitleOffset(1.3);
+	h_recosliceid_recoinelastic_cuts->GetYaxis()->SetRangeUser(ymin_recoinel,ymax_recoinel);
+	h_recosliceid_recoinelastic_cuts->Draw();
+
 	h_recosliceid_inelastic_cuts->SetLineColor(2);
+	h_recosliceid_inelastic_cuts->SetLineWidth(4);
+	//h_recosliceid_inelastic_cuts->SetFillColor(2);
+	//h_recosliceid_inelastic_cuts->SetFillStyle(3354);
 	h_recosliceid_inelastic_cuts->Draw("same hist");
+	hint->DrawCopy("same");
 	//TLegend *leg3 = new TLegend(0.5,0.6,0.8,0.9);
-	TLegend *leg3 = new TLegend(0.5-0.3,0.6,0.8-0.3,0.9);
+	//TLegend *leg3 = new TLegend(0.5,0.6,0.8,0.9);
+	TLegend *leg3 = new TLegend(0.45,0.55,0.88,0.9);
 	leg3->SetFillStyle(0);
 	leg3->AddEntry(h_recosliceid_allevts_cuts,"Selected","ple");
 	leg3->AddEntry(hint, "Selected #times purity","ple");
@@ -109,18 +141,24 @@ void makeXS(TString fin, TString outpath){
 	leg3->Draw();
 
 	TCanvas *c4 = new TCanvas("c4","c4");
-	h_truesliceid_inelastic_uf->SetLineColor(4);
-	h_truesliceid_inelastic_uf->SetMarkerColor(4);
-	h_truesliceid_inelastic_uf->SetTitle("Proton Inelastic Scatterings; True SliceID; Events");
+	h_truesliceid_inelastic_uf->SetLineColor(7);
+	h_truesliceid_inelastic_uf->SetMarkerColor(7);
+	//h_truesliceid_inelastic_uf->SetTitle("Proton Inelastic Scatterings; True SliceID; Events");
+	h_truesliceid_inelastic_uf->SetTitle("; True SliceID; Events");
+	h_truesliceid_inelastic_uf->GetYaxis()->SetRangeUser(0,3800);
+	h_truesliceid_inelastic_uf->GetYaxis()->SetTitleOffset(1.3);
 	h_truesliceid_inelastic_uf->Draw();
 	//hint->SetLineColor(3);
 	//hint->SetMarkerColor(3);
-	hint->DrawCopy("same");
 	h_truesliceid_inelastic_all->SetLineColor(2);
+	h_truesliceid_inelastic_all->SetLineWidth(4);
 	h_truesliceid_inelastic_all->SetMarkerColor(2);
 	h_truesliceid_inelastic_all->Draw("same hist");
+	hint->DrawCopy("same");
+
 	//TLegend *leg4 = new TLegend(0.5,0.6,0.8,0.9);
-	TLegend *leg4 = new TLegend(0.5-0.3,0.6,0.8-0.3,0.9);
+	//TLegend *leg4 = new TLegend(0.5,0.6,0.8,0.9);
+	TLegend *leg4 = new TLegend(0.35,0.56,0.85,0.9);
 	leg4->SetFillStyle(0);
 	leg4->AddEntry(hint, "Selected #times purity","ple");
 	leg4->AddEntry(h_truesliceid_inelastic_uf,"Unfolded protons","ple");
@@ -150,8 +188,8 @@ void makeXS(TString fin, TString outpath){
 	TCanvas *c6 = new TCanvas("c6","c6");
 	gr_inc->SetTitle("");
 	gr_inc->SetLineWidth(2);
-	gr_inc->SetLineColor(4);
-	gr_inc->SetMarkerColor(4);
+	gr_inc->SetLineColor(7);
+	gr_inc->SetMarkerColor(7);
 	gr_inc->GetXaxis()->SetTitle("Slice ID");
 	gr_inc->GetYaxis()->SetTitle("N_{Inc}");
 	gr_inc->GetXaxis()->SetRangeUser(-1, 23);
@@ -160,8 +198,8 @@ void makeXS(TString fin, TString outpath){
 	TCanvas *c7 = new TCanvas("c7","c7");
 	gr_int->SetTitle("");
 	gr_int->SetLineWidth(2);
-	gr_int->SetLineColor(4);
-	gr_int->SetMarkerColor(4);
+	gr_int->SetLineColor(7);
+	gr_int->SetMarkerColor(7);
 	gr_int->GetXaxis()->SetTitle("Slice ID");
 	gr_int->GetYaxis()->SetTitle("N_{Int}");
 	gr_int->GetXaxis()->SetRangeUser(-1, 23);
@@ -189,36 +227,62 @@ void makeXS(TString fin, TString outpath){
 		//std::cout<<i<<" "<<Ninc[i]<<" "<<Nint[i]<<" "<<xs[i]<<" "<<incE[i]<<std::endl;
 	}
 
+
+
+
 	TFile f2("/dune/data2/users/hyliao/GeantReweight/xs_cascade/proton_cross_section.root");
 	TGraph *total_inel_KE = (TGraph*)f2.Get("inel_KE");
 
 	TGraphErrors *gr_recoxs = new TGraphErrors(nthinslices, incE, xs, 0, err_xs);
+	gStyle->SetPadLeftMargin(0.13);
 	TCanvas *c5 = new TCanvas("c5", "c5");
-	TH2D *f2d=new TH2D("f2d","",450,0,450,1500,0,1500);
+	float ymax=1500;
+	float xmax=450;
+	float xmin=0;
+	TH2D *f2d=new TH2D("f2d","",450,xmin,xmax,ymax,0,ymax);
 	f2d->GetXaxis()->SetTitle("Proton Kinetic Energy [MeV]");
-	f2d->GetYaxis()->SetTitle("#sigma_{inelastic} [mb]");
+	f2d->GetYaxis()->SetTitleOffset(1.3);
+	//f2d->GetYaxis()->SetTitle("#sigma_{inelastic} [mb]");
+	f2d->GetYaxis()->SetTitle("P-Ar inelastic cross section [mb]");
 	f2d->Draw("");
 
 	//gr_recoxs->SetTitle("Proton Inelastic Cross Section");
 	//gr_recoxs->GetXaxis()->SetTitle("Proton Kinetic Energy (MeV)");
 	//gr_recoxs->GetYaxis()->SetTitle("#sigma_{inelastic} (mb)");
+	total_inel_KE->SetLineColor(2);
+	total_inel_KE->Draw("c same");
 	gr_recoxs->SetLineWidth(2);
+	//gr_recoxs->SetEndErrorSize(0);
 	gr_recoxs->Draw("p same");
 	//gr_recoxs->Draw("ape same");
 	gr_truexs->SetMarkerColor(3);
 	gr_truexs->SetLineColor(3);
 	//gr_truexs->Draw("pe same");
 	gr_truexs->Draw("p same");
-	total_inel_KE->SetLineColor(2);
-	total_inel_KE->Draw("c same");
 
-	TLegend *leg5 = new TLegend(0.3,0.6,0.8,0.9);
+	TLegend *leg5 = new TLegend(0.3,0.55,0.8,0.85);
 	leg5->SetFillStyle(0);
 	leg5->AddEntry(gr_recoxs, "MC with reco information", "pe");
 	leg5->AddEntry(gr_truexs, "MC with truth information", "pe");
 	//leg5->AddEntry(total_inel_KE, "Geant4 v4_10_6_p01c", "l");
 	leg5->AddEntry(total_inel_KE, "Geant4", "l");
 	leg5->Draw();
+
+   	//pDUNE Logo
+        float logo_y=ymax+15;
+        TLatex **txt_pdune1=new TLatex*[1];
+        txt_pdune1[0]=new TLatex(xmin, logo_y, Form("#bf{DUNE:ProtoDUNE-SP}"));
+        txt_pdune1[0]->SetTextColor(1);
+        txt_pdune1[0]->Draw();
+        //
+        //Beam Logo
+        TLatex **txt_p1=new TLatex*[1];
+        //txt_p1[0]=new TLatex(xmax-6.3, logo_y, Form("Protons (1 GeV/c)")); //x:0-20
+        txt_p1[0]=new TLatex(xmax-150, logo_y, Form("Protons (1 GeV/c)")); //x:0-40
+        txt_p1[0]->SetTextColor(1);
+        txt_p1[0]->SetTextSize(0.05);
+        txt_p1[0]->Draw();
+
 
 	//c1->Print("plots/xs_sliceidinc_reco.eps");
 	//c2->Print("plots/xs_sliceidinc_true.eps");
