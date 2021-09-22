23f4c487dcb9883ff382c44cb2cb76edd55e1479
Heng-Ye Liao <hyliao@dunegpvm08.fnal.gov>
2021-09-21 22:25:34 -0500

    Range reco vs true and ntrklen vs chi2PID as a func. of cosTheta

--- headers/MisIDP.h
+++ headers/MisIDP.h
@@ -17,9 +17,9 @@ void SetOutputFileName(std::string name){fOutputFileName = name;};
 void BookHistograms();
 void SaveHistograms();
 
-TH2D *h2d_recotrklen_truetrklen_inel;
-TH2D *h2d_recotrklen_truetrklen_el;
-TH2D *h2d_recotrklen_truetrklen_misidp;
+TProfile2D *h2d_recotrklen_truetrklen_inel;
+TProfile2D *h2d_recotrklen_truetrklen_el;
+TProfile2D *h2d_recotrklen_truetrklen_misidp;
 
 TH3D *h3d_recotrklen_truetrklen_cosTheta_inel;
 TH3D *h3d_recotrklen_truetrklen_cosTheta_el;
@@ -27,15 +27,26 @@ TH3D *h3d_recotrklen_truetrklen_cosTheta_misidp;
 
 
 //cosTheta
-TH2D *h2d_ntrklen_chi2_inel;
-TH2D *h2d_ntrklen_chi2_el;
-TH2D *h2d_ntrklen_chi2_misidp;
+TProfile2D *h2d_ntrklen_chi2_inel;
+TProfile2D *h2d_ntrklen_chi2_el;
+TProfile2D *h2d_ntrklen_chi2_misidp;
 
 TH3D *h3d_ntrklen_chi2_cosTheta_inel;
 TH3D *h3d_ntrklen_chi2_cosTheta_el;
 TH3D *h3d_ntrklen_chi2_cosTheta_misidp;
 
 
+//cosTheta_slicing
+const int nn_cos=10;
+//track length
+TProfile2D *tp2d_range_reco_true_inel[nn_cos];
+TProfile2D *tp2d_range_reco_true_el[nn_cos];
+TProfile2D *tp2d_range_reco_true_misidp[nn_cos];
+
+//ntrklen vs chi2pid
+TProfile2D *tp2d_ntrklen_chi2_inel[nn_cos];
+TProfile2D *tp2d_ntrklen_chi2_el[nn_cos];
+TProfile2D *tp2d_ntrklen_chi2_misidp[nn_cos];
 
 
 void BookHistograms() { //BookHistograms
@@ -49,21 +60,44 @@ void BookHistograms() { //BookHistograms
 	double cos_min=0;
 	double cos_max=1;
 
-	h2d_recotrklen_truetrklen_inel=new TH2D("h2d_recotrklen_truetrklen_inel","",n_2d,trklen_min,trklen_max,n_2d,trklen_min,trklen_max);
-	h2d_recotrklen_truetrklen_el=new TH2D("h2d_recotrklen_truetrklen_el","",n_2d,trklen_min,trklen_max,n_2d,trklen_min,trklen_max);
-	h2d_recotrklen_truetrklen_misidp=new TH2D("h2d_recotrklen_truetrklen_misidp","",n_2d,trklen_min,trklen_max,n_2d,trklen_min,trklen_max);
+	h2d_recotrklen_truetrklen_inel=new TProfile2D("h2d_recotrklen_truetrklen_inel","",n_2d, trklen_min, trklen_max, n_2d, trklen_min, trklen_max);
+	h2d_recotrklen_truetrklen_el=new TProfile2D("h2d_recotrklen_truetrklen_el","",n_2d, trklen_min, trklen_max, n_2d, trklen_min, trklen_max);
+	h2d_recotrklen_truetrklen_misidp=new TProfile2D("h2d_recotrklen_truetrklen_misidp","",n_2d, trklen_min, trklen_max, n_2d, trklen_min, trklen_max);
+
+	
+	h2d_recotrklen_truetrklen_inel->SetTitle("inel; Reco Track Length[cm]; True Track Length[cm]; cos#Theta");
+	h2d_recotrklen_truetrklen_el->SetTitle("El; Reco Track Length[cm]; True Track Length[cm]; cos#Theta");
+	h2d_recotrklen_truetrklen_misidp->SetTitle("MisID:p; Reco Track Length[cm]; True Track Length[cm]; cos#Theta");
 
 	h2d_recotrklen_truetrklen_inel->Sumw2();
 	h2d_recotrklen_truetrklen_el->Sumw2();
 	h2d_recotrklen_truetrklen_misidp->Sumw2();
 
-	h3d_recotrklen_truetrklen_cosTheta_inel=new TH3D("h3d_recotrklen_truetrklen_cosTheta_inel","",n_2d,trklen_min,trklen_max,n_2d,trklen_min,trklen_max,n_cos,cos_min,cos_max);
-	h3d_recotrklen_truetrklen_cosTheta_el=new TH3D("h3d_recotrklen_truetrklen_cosTheta_el","",n_2d,trklen_min,trklen_max,n_2d,trklen_min,trklen_max,n_cos,cos_min,cos_max);
-	h3d_recotrklen_truetrklen_cosTheta_misidp=new TH3D("h3d_recotrklen_truetrklen_cosTheta_misidp","",n_2d,trklen_min,trklen_max,n_2d,trklen_min,trklen_max,n_cos,cos_min,cos_max);
+	h3d_recotrklen_truetrklen_cosTheta_inel=new TH3D("h3d_recotrklen_truetrklen_cosTheta_inel","",n_2d, trklen_min, trklen_max, n_2d, trklen_min, trklen_max, n_cos, cos_min, cos_max);
+	h3d_recotrklen_truetrklen_cosTheta_el=new TH3D("h3d_recotrklen_truetrklen_cosTheta_el","",n_2d, trklen_min, trklen_max, n_2d, trklen_min, trklen_max, n_cos, cos_min,cos_max);
+	h3d_recotrklen_truetrklen_cosTheta_misidp=new TH3D("h3d_recotrklen_truetrklen_cosTheta_misidp","",n_2d, trklen_min, trklen_max, n_2d, trklen_min, trklen_max, n_cos, cos_min, cos_max);
+
+	h3d_recotrklen_truetrklen_cosTheta_inel->SetTitle("inel; Reco Track Length[cm]; True Track Length[cm]; cos#Theta");
+	h3d_recotrklen_truetrklen_cosTheta_el->SetTitle("inel; Reco Track Length[cm]; True Track Length[cm]; cos#Theta");
+	h3d_recotrklen_truetrklen_cosTheta_misidp->SetTitle("inel; Reco Track Length[cm]; True Track Length[cm]; cos#Theta");
 
 	h3d_recotrklen_truetrklen_cosTheta_inel->Sumw2();
 	h3d_recotrklen_truetrklen_cosTheta_el->Sumw2();
 	h3d_recotrklen_truetrklen_cosTheta_misidp->Sumw2();
+
+	float dcos=0.1;
+	for (int j=0; j<nn_cos; ++j) {
+		float tmp_min=(float)j*dcos;
+		float tmp_max=tmp_min+dcos;
+		tp2d_range_reco_true_inel[j]=new TProfile2D(Form("tp2d_range_reco_true_inel_%d",j), Form("Cos#Theta:%.1f-%.1f",tmp_min,tmp_max), n_2d, trklen_min, trklen_max, n_2d, trklen_min, trklen_max);
+		tp2d_range_reco_true_el[j]=new TProfile2D(Form("tp2d_range_reco_true_el_%d",j), Form("Cos#Theta:%.1f-%.1f",tmp_min,tmp_max), n_2d, trklen_min, trklen_max, n_2d, trklen_min, trklen_max);
+		tp2d_range_reco_true_misidp[j]=new TProfile2D(Form("tp2d_range_reco_true_misidp_%d",j), Form("Cos#Theta:%.1f-%.1f",tmp_min,tmp_max), n_2d, trklen_min, trklen_max, n_2d, trklen_min, trklen_max);
+
+		tp2d_range_reco_true_inel[j]->Sumw2();
+		tp2d_range_reco_true_el[j]->Sumw2();
+		tp2d_range_reco_true_misidp[j]->Sumw2();
+	}
+
 
         int n_chi2=300;
 	double chi2_min=0;
@@ -73,21 +107,45 @@ void BookHistograms() { //BookHistograms
 	double ntrllen_min=0;
 	double ntrklen_max=1.2;
 	
-	h2d_ntrklen_chi2_inel=new TH2D("h2d_ntrklen_chi2_inel","",n_ntrklen,ntrllen_min,ntrklen_max,n_chi2,chi2_min,ntrklen_max);
-	h2d_ntrklen_chi2_el=new TH2D("h2d_ntrklen_chi2_el","",n_ntrklen,ntrllen_min,ntrklen_max,n_chi2,chi2_min,ntrklen_max);
-	h2d_ntrklen_chi2_misidp=new TH2D("h2d_ntrklen_chi2_misidp","",n_ntrklen,ntrllen_min,ntrklen_max,n_chi2,chi2_min,ntrklen_max);
+	h2d_ntrklen_chi2_inel=new TProfile2D("h2d_ntrklen_chi2_inel","", n_ntrklen, ntrllen_min, ntrklen_max, n_chi2, chi2_min, chi2_max);
+	h2d_ntrklen_chi2_el=new TProfile2D("h2d_ntrklen_chi2_el","", n_ntrklen, ntrllen_min, ntrklen_max, n_chi2, chi2_min, chi2_max);
+	h2d_ntrklen_chi2_misidp=new TProfile2D("h2d_ntrklen_chi2_misidp","",n_ntrklen, ntrllen_min, ntrklen_max, n_chi2, chi2_min, chi2_max);
+
+	h2d_ntrklen_chi2_inel->SetTitle("inel; Proton Track Length/CSDA; #chi^{2} PID; cos#Theta");
+	h2d_ntrklen_chi2_el->SetTitle("inel; Proton Track Length/CSDA; #chi^{2} PID; cos#Theta");
+	h2d_ntrklen_chi2_misidp->SetTitle("inel; Proton Track Length/CSDA; #chi^{2} PID; cos#Theta");
 
 	h2d_ntrklen_chi2_inel->Sumw2();
 	h2d_ntrklen_chi2_el->Sumw2();
 	h2d_ntrklen_chi2_misidp->Sumw2();
 
-	h3d_ntrklen_chi2_cosTheta_inel=new TH2D("h2d_ntrklen_chi2_cosTheta_inel","",n_ntrklen,ntrllen_min,ntrklen_max,n_chi2,chi2_min,ntrklen_max,n_chi2,chi2_min,chi2_max);
-	h3d_ntrklen_chi2_cosTheta_el=new TH2D("h2d_ntrklen_chi2_cosTheta_el","",n_ntrklen,ntrllen_min,ntrklen_max,n_chi2,chi2_min,ntrklen_max,n_chi2,chi2_min,chi2_max);
-	h3d_ntrklen_chi2_cosTheta_misidp=new TH2D("h2d_ntrklen_chi2_cosTheta_misidp","",n_ntrklen,ntrllen_min,ntrklen_max,n_chi2,chi2_min,ntrklen_max,n_chi2,chi2_min,chi2_max);
+	h3d_ntrklen_chi2_cosTheta_inel=new TH3D("h2d_ntrklen_chi2_cosTheta_inel","", n_ntrklen, ntrllen_min, ntrklen_max, n_chi2, chi2_min, chi2_max, n_cos, cos_min, cos_max);
+	h3d_ntrklen_chi2_cosTheta_el=new TH3D("h2d_ntrklen_chi2_cosTheta_el","", n_ntrklen, ntrllen_min, ntrklen_max, n_chi2, chi2_min, chi2_max, n_cos, cos_min, cos_max);
+	h3d_ntrklen_chi2_cosTheta_misidp=new TH3D("h2d_ntrklen_chi2_cosTheta_misidp","", n_ntrklen, ntrllen_min, ntrklen_max, n_chi2, chi2_min, chi2_max, n_cos, cos_min, cos_max);
+
+	h3d_ntrklen_chi2_cosTheta_inel->SetTitle("inel; Proton Track Length/CSDA; #chi^{2} PID; cos#Theta");
+	h3d_ntrklen_chi2_cosTheta_el->SetTitle("inel; Proton Track Length/CSDA; #chi^{2} PID; cos#Theta");
+	h3d_ntrklen_chi2_cosTheta_misidp->SetTitle("inel; Proton Track Length/CSDA; #chi^{2} PID; cos#Theta");
 
 	h3d_ntrklen_chi2_cosTheta_inel->Sumw2();
 	h3d_ntrklen_chi2_cosTheta_el->Sumw2();
 	h3d_ntrklen_chi2_cosTheta_misidp->Sumw2();
+
+	
+	for (int j=0; j<nn_cos; ++j) {
+		float tmp_min=(float)j*dcos;
+		float tmp_max=tmp_min+dcos;
+
+		tp2d_ntrklen_chi2_inel[j]=new TProfile2D(Form("tp2d_ntrklen_chi2_inel_%d", j), Form("Cos#Theta:%.1f-%.1f",tmp_min,tmp_max), n_ntrklen, ntrllen_min, ntrklen_max, n_chi2, chi2_min, chi2_max);
+		tp2d_ntrklen_chi2_el[j]=new TProfile2D(Form("tp2d_ntrklen_chi2_el_%d", j), Form("Cos#Theta:%.1f-%.1f",tmp_min,tmp_max), n_ntrklen, ntrllen_min, ntrklen_max, n_chi2, chi2_min, chi2_max);
+		tp2d_ntrklen_chi2_misidp[j]=new TProfile2D(Form("tp2d_ntrklen_chi2_misidp_%d", j), Form("Cos#Theta:%.1f-%.1f",tmp_min,tmp_max), n_ntrklen, ntrllen_min, ntrklen_max, n_chi2, chi2_min, chi2_max);
+
+
+		tp2d_ntrklen_chi2_inel[j]->Sumw2();
+		tp2d_ntrklen_chi2_el[j]->Sumw2();
+		tp2d_ntrklen_chi2_misidp[j]->Sumw2();
+	}
+
 
 } //BookHistograms
 
