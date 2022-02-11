4b1943fcfdb437991a5b96d8631378fd121164dc
Heng-Ye Liao <hyliao@dunegpvm09.fnal.gov>
2022-02-11 13:20:31 -0600

    Add configs. for E-slice

---
+++ headers/ESliceParams.h
@@ -0,0 +1,13 @@
+
+	const int nthinslices=25; //total # of slices
+	const double thinslicewidth=20; //MeV
+	const int name_thinslicewidth=20; //MeV
+
+	const double Emin=0; //emin
+	const double Emax=500; //emax
+
+
+	Int_t nbinse=120; //nbins for KE 
+	//Int_t nbinse=12; //nbins for KE 
+	Int_t nbinsthickness = 1200; //nbins for trk pitch
+
