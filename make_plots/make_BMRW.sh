c24983a0b364cb696fa13899397c307568666809
Heng-Ye Liao <liao@phys.ksu.edu>
2022-07-02 14:07:18 -0500

    Revise the reweighting func. definition

--- make_plots/make_BMRW.sh
+++ make_plots/make_BMRW.sh
@@ -4,9 +4,18 @@ file_bmrw_data="/dune/app/users/hyliao/WORK/analysis/protodune/proton/analysis/r
 file_bmrw_mc="/dune/app/users/hyliao/WORK/analysis/protodune/proton/analysis/mcdata/sce/MC_PDSPProd4a_MC_1GeV_reco1_sce_datadriven_v1/xs_thinslice/rw/mc_proton_beamxy_beammom_bmrw.root"
 fout_path="./plots_beamxy_bmrw"
 
-#exe2_str="root -b -q 'plotBMRW.C(\""$file_bmrw_data\"", \""$file_bmrw_mc\"", \""$fout_path\"")'"
-#exe2_str="root -b -q 'plotDataMCKE.C(\""$file_bmrw_data\"", \""$file_bmrw_mc\"", \""$fout_path\"")'"
-exe2_str="root -l 'plotDataMCKE.C(\""$file_bmrw_data\"", \""$file_bmrw_mc\"", \""$fout_path\"")'"
+#exe1_str="root -b -q 'plotBMRW.C(\""$file_bmrw_data\"", \""$file_bmrw_mc\"", \""$fout_path\"")'"
+#exe1_str="root -b -q 'plotDataMCKE.C(\""$file_bmrw_data\"", \""$file_bmrw_mc\"", \""$fout_path\"")'"
 
 
+#draw mean and sigma for data and MC 
+exe1_str="root -l 'plotDataMCKE.C(\""$file_bmrw_data\"", \""$file_bmrw_mc\"", \""$fout_path\"")'"
+
+#calculate bmrw
+exe2_str="root -b -q 'plotBMRW.C(\""$file_bmrw_data\"", \""$file_bmrw_mc\"", \""$fout_path\"")'"
+
+
+
+
+#eval $exe1_str
 eval $exe2_str
