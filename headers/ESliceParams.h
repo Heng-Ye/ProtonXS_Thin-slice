c9eaee8022df6fc3db511757ebd5782c31f4936f
Heng-Ye Liao <liao@phys.ksu.edu>
2023-02-10 22:21:32 -0600

    Add new config. of E-slice

--- headers/ESliceParams.h
+++ headers/ESliceParams.h
@@ -11,11 +11,24 @@
 	//const double Emin=0; //emin
 	//const double Emax=600; //emax
 
-        const int nthinslices=40; //total # of slices
-        const double thinslicewidth=20; //MeV
-        const int name_thinslicewidth=20; //MeV
+        //const int nthinslices=40; //total # of slices
+        //const double thinslicewidth=20; //MeV
+        //const int name_thinslicewidth=20; //MeV
+        //const double Emin=0; //emin
+        //const double Emax=800; //emax
+
+        const int nthinslices=24; //total # of slices
+        const double thinslicewidth=25; //MeV
+        const int name_thinslicewidth=24; //MeV
         const double Emin=0; //emin
-        const double Emax=800; //emax
+        const double Emax=600; //emax
+
+
+        //const int nthinslices=10; //total # of slices
+        //const double thinslicewidth=80; //MeV
+        //const int name_thinslicewidth=80; //MeV
+        //const double Emin=0; //emin
+        //const double Emax=800; //emax
 
         //const int nthinslices=20; //total # of slices
         //const double thinslicewidth=30; //MeV
